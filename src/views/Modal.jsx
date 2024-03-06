3b66969ddfdb0be476eda7d356b8fe730fc51394
Joaquin Vazquez Garcia <joaquinvazquezgarcia01@gmail.com>
2024-03-06 17:26:20 +0100

    el Create del crud ya funciona, falta validar los campos

--- src/views/Modal.jsx
+++ src/views/Modal.jsx
@@ -1,16 +1,46 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import { useEffect } from "react";
+
+let nextId = 0;
 
 function FormularioModal() {
+    const [recipiesArray, setrecipiesArray] = useState([]);
+    const [title, setTitle] = useState("");
+    const [url, setUrl] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const object = {
+            id: nextId++,
+            title: title,
+            url: url,
+            description: description,
+        };
+
+        setrecipiesArray([...recipiesArray, object]);
+        setTitle("");
+        setUrl("");
+        setDescription("");
+    };
+
+    useEffect(() => {
+        localStorage.setItem("recetas", JSON.stringify(recipiesArray));
+    }, [recipiesArray]);
+
     return (
         <Form className="my-5 d-flex flex-column">
             <Form.Group className="mb-3" controlId="form.TituloInput">
-                <Form.Label>Título Receta</Form.Label>
+                <Form.Label>Nombre de la Receta</Form.Label>
                 <Form.Control
                     type="text"
                     maxLength={40}
-                    placeholder="Título de ejemplo"
+                    placeholder="Ej: Tarta de Coco y Nuez"
                     required
+                    onChange={e => setTitle(e.target.value)}
+                    value={title}
                 />
             </Form.Group>
             <Form.Group className="mb-3" controlId="form.UrlInput">
@@ -20,18 +50,27 @@ function FormularioModal() {
                     maxLength={100}
                     placeholder="Ingrese la URL de su imagen"
                     required
+                    onChange={e => setUrl(e.target.value)}
+                    value={url}
                 />
             </Form.Group>
             <Form.Group className="mb-3" controlId="form.DescripcionInput">
-                <Form.Label>Descripción de la receta</Form.Label>
+                <Form.Label>Descripción de la Receta</Form.Label>
                 <Form.Control
                     type="text"
                     maxLength={2000}
-                    placeholder="Título de ejemplo"
+                    placeholder="Descripción de la receta"
                     required
+                    onChange={e => setDescription(e.target.value)}
+                    value={description}
                 />
             </Form.Group>
-            <Button className="col-2 h-75 my-auto ms-auto" variant="primary">
+            <Button
+                className="col-2 h-75 my-auto ms-auto"
+                type="submit"
+                variant="primary"
+                onClick={handleSubmit}
+            >
                 +Agregar
             </Button>{" "}
         </Form>
