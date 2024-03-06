450a0dcb29f1f94ad4125d69a7cbaf8b99d113d1
Joaquin Vazquez Garcia <joaquinvazquezgarcia01@gmail.com>
2024-03-06 13:57:17 +0100

    estilos de los links arreglado y el modal ya no es modal

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import VentanaModal from "./Modal";
 import TablaRecetas from "./TablaRecetas";
-
+import FormularioModal from "./Modal";
 
 export const Admin = () => {
     return (
         <main className="adminMain container">
+            <FormularioModal></FormularioModal>
             <section className="encabezadoAdmin container row my-3">
                 <h1 className="col-10">Administración del sitio</h1>
-                <Button className="col-2 h-75 my-auto" variant="primary">+Agregar</Button>{' '}
             </section>
             <section className="container mb-3">
                 <TablaRecetas></TablaRecetas>
@@ -17,4 +15,3 @@ export const Admin = () => {
         </main>
     );
 };
-
--- src/views/Modal.jsx
+++ src/views/Modal.jsx
@@ -1,65 +1,41 @@
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function FormularioModal() {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="form.TituloInput">
-        <Form.Label>Título Receta</Form.Label>
-        <Form.Control
-          type="text"
-          maxLength={40}
-          placeholder="Título de ejemplo"
-          required
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="form.UrlInput">
-        <Form.Label>URL de la imagen</Form.Label>
-        <Form.Control
-          type="text"
-          maxLength={100}
-          placeholder="Ingrese la URL de su imagen"
-          required
-        />
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="form.DescripcionInput">
-        <Form.Label>Descripción de la receta</Form.Label>
-        <Form.Control
-          type="text"
-          maxLength={2000}
-          placeholder="Título de ejemplo"
-          required
-        />
-      </Form.Group>
-    </Form>
-  );
+    return (
+        <Form className="my-5 d-flex flex-column">
+            <Form.Group className="mb-3" controlId="form.TituloInput">
+                <Form.Label>Título Receta</Form.Label>
+                <Form.Control
+                    type="text"
+                    maxLength={40}
+                    placeholder="Título de ejemplo"
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="form.UrlInput">
+                <Form.Label>URL de la imagen</Form.Label>
+                <Form.Control
+                    type="text"
+                    maxLength={100}
+                    placeholder="Ingrese la URL de su imagen"
+                    required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="form.DescripcionInput">
+                <Form.Label>Descripción de la receta</Form.Label>
+                <Form.Control
+                    type="text"
+                    maxLength={2000}
+                    placeholder="Título de ejemplo"
+                    required
+                />
+            </Form.Group>
+            <Button className="col-2 h-75 my-auto ms-auto" variant="primary">
+                +Agregar
+            </Button>{" "}
+        </Form>
+    );
 }
 
-function VentanaModal() {
-  return (
-    <div
-      className="modal show"
-      style={{ display: "block", position: "initial" }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Administración del sitio</Modal.Title>
-        </Modal.Header>
-       
-        <Modal.Body>
-           <FormularioModal />
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary">Cerrar</Button>
-          <Button variant="primary">Guardar cambios</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>
-  );
-}
-
-export default VentanaModal;
+export default FormularioModal;
