3ce63b38ef552a18cf8a49f0d33c99f70a207687
Joaquin Vazquez Garcia <joaquinvazquezgarcia01@gmail.com>
2024-03-06 19:17:50 +0100

    Funciona inicio, create y delete

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -3,33 +3,13 @@ import CardInicio from "./CardInicio";
 import CardBanner from "./CardBanner";
 import "../css/inicio.css";
 
-
-/* Eliminar despues es solo para que se muestre algo */
-const objetoDePrueba = {
-    title: "Objeto de Prueba",
-    description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias ullam delectus reprehenderit tenetur non ut alias fugit odio, tempora amet sit ratione numquam. Impedit aspernatur incidunt voluptate deleniti tenetur?",
-    img: "https://www.cbc.ca/kidsnews/images/GOOGLE25THTHUMB2.png",
-};
-
-/* Esto sería como el array que viene de localStorage */
-const arrayDePrueba = [
-    objetoDePrueba,
-    objetoDePrueba,
-    objetoDePrueba,
-    objetoDePrueba,
-    objetoDePrueba,
-    objetoDePrueba,
-    objetoDePrueba,
-];
-
 export const Inicio = () => {
     /* Acá iria la logica para obtener el array de localStorage */
     const [arrayRecetas, setRecetas] = useState([]);
 
     useEffect(() => {
         const obtenerObjetosLocalStorage = () => {
-            const arrayLocalStorage = localStorage.getItem("tuClaveLocalStorage");
+            const arrayLocalStorage = localStorage.getItem("recetas");
             if (arrayLocalStorage) {
                 const arrayRecetas = JSON.parse(arrayLocalStorage);
                 setRecetas(arrayRecetas);
@@ -41,14 +21,19 @@ export const Inicio = () => {
     return (
         <main className="inicioMain">
             <section className="inicioBanner container">
-                <CardBanner props={arrayRecetas[0]} />
+                {arrayRecetas.length > 0 ? (
+                    <CardBanner props={arrayRecetas[0]} />
+                ) : (
+                    <h2 className="text-center my-5">
+                        Hola ¡todavia no hay recetas disponibles!
+                    </h2>
+                )}
             </section>
             <section className="inicioMenuRecetas d-flex justify-content-between flex-wrap container">
-                {arrayRecetas.map((object) => (
-                    <CardInicio props={object} />
-                ))}
+                {arrayRecetas.length > 0
+                    ? arrayRecetas.map(object => <CardInicio props={object} />)
+                    : ""}
             </section>
         </main>
     );
-
 };
